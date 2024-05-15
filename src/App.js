cd24b709b670c99e4389a5d3bd4b2bb17836237d
Ethan <ethanw263@gmail.com>
2024-05-15 10:16:55 -0700

    Using React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import Home from './pages/Home';
+import Nav from './components/Nav'
+import Footer from './components/Footer';
+import Books from './pages/Books';
+import { books } from './data'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home}>
+          <Home />
+        </Route>
+        <Route path="/books" render={() => <Books books={books} />}>
+          <Books />
+        </Route>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
